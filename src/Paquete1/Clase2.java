032bb91f9299e57b982954a0c28905657bfec1b7
Paul Cabrera <picabreras@grupodelaware.com>
2022-07-20 15:57:31 -0500

    Mi primer commit

---
+++ src/Paquete1/Clase2.java
@@ -0,0 +1,10 @@
+package Paquete1;
+
+public class Clase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Paquete2/Clase3.java
@@ -0,0 +1,10 @@
+package Paquete2;
+
+public class Clase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
